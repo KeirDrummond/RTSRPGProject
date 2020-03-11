856fd6d4c5613c69b6bfc75beae1141abda5b066
KeirDrummond <k.drummond2@uni.brighton.ac.uk>
2020-03-11 20:40:18 +0000

    Created classes to provide more shared and customisable functionality between player classes.

---
+++ Source/RTSRPGProject/ProjectAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ProjectAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTSRPGPROJECT_API AProjectAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
