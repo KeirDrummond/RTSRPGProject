3c079b02f979144c9120e4a124f49b6425251d12
KeirDrummond <k.drummond2@uni.brighton.ac.uk>
2020-03-22 14:59:56 +0000

    Added game state class to replace some things stored in game mode

---
+++ Source/RTSRPGProject/ProjectGameState.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "ProjectGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTSRPGPROJECT_API AProjectGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+public:
+
+protected:
+
+};
